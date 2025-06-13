65d5b71eab9deb4856bbb2b91a27d495ad2e2718
Maxwell Schmitt <maxwschm@cisco.com>
2025-06-13 12:57:34 -0400

    Update to 2025.2

--- setup.sh
+++ setup.sh
@@ -10,8 +10,6 @@ env
 # Wait for possible auto updates to complete.  This may not be needed
 flock -w 120 /var/lib/apt/lists/lock -c 'echo waiting for lock'
 
-wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/kali-archive-keyring.gpg
-
 apt-get update
 apt-get upgrade -y
 
